1bdcd5634f75eea25802ff621ac7041cd5f70ea2
walterpaulo <walter0paulo@hotmail.com>
2022-05-05 12:52:34 -0300

    feat: added id

--- app/models/user_tdo.rb
+++ app/models/user_tdo.rb
@@ -1,7 +1,8 @@
 class UserTdo
-    attr_reader :name, :email
+    attr_reader :id, :name, :email
 
-    def initialize(name:, email:)
+    def initialize(id:, name:, email:)
+        @id = id
         @name = name
         @email = email
     end
