ec54c6c45529c4219dcbce08f538ebf5dfe39207
walterpaulo <walter0paulo@hotmail.com>
2022-05-05 06:04:24 -0300

    feat: validates user

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,9 @@
 class Usuario < ApplicationRecord
+  before_save { self.email = email.downcase }
+  before_save { self.password = BCrypt::Password.create(password) }
+  validates :email, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  }
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
